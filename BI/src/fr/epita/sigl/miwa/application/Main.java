addff300a97b63b65f23b270a85340b0661c0996
BI-Laura <ollivier.la@gmail.com>
2013-12-17 15:45:52 +0100

    Mock BI

--- BI/src/fr/epita/sigl/miwa/application/Main.java
+++ BI/src/fr/epita/sigl/miwa/application/Main.java
@@ -1,7 +1,9 @@
 package fr.epita.sigl.miwa.application;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import fr.epita.sigl.miwa.application.clock.ClockClient;
 import fr.epita.sigl.miwa.application.messaging.AsyncMessageListener;
 import fr.epita.sigl.miwa.application.messaging.SyncMessHandler;
 import fr.epita.sigl.miwa.st.Conf;
@@ -23,14 +25,6 @@ public class Main {
 				.initListener(new AsyncMessageListener());
 		/* !ST DO NOT REMOVE/MODIFY OR PUT ANYTHING ABOVE */
 		/* CODE HERE */
-		try {
-			Thread.sleep(40000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		SyncMessHandler.getSyncMessSender().sendMessage(
-				EApplication.BI, "Coucou BI");
 		/* !CODE HERE */
 		/* ST DO NOT REMOVE/MODIFY OR PUT ANYTHING BELOW */
 		AsyncMessageFactory.getInstance().getAsyncMessageManager()
