0ab1a9523ae535e209dbabfe43f36bd4c8f75fea
Romain PASCAL <pascal.romain78@gmail.com>
2014-01-17 00:39:38 +0100

    Clients

---
+++ CRM/src/fr/epita/sigl/miwa/application/crm/Clients.java
@@ -0,0 +1,15 @@
+package fr.epita.sigl.miwa.application.crm;
+
+import java.util.List;
+
+public class Clients {
+
+	private List<Client> clients;
+	
+	public List<Client> getClients() {
+		return clients;
+	}
+	public void setClients(List<Client> clients) {
+		this.clients = clients;
+	}
+}
