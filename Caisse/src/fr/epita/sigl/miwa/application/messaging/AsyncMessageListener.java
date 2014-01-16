d820cdd48799296235e49f190ff841005e39489e
Fabien Patou <fabien.patou.junk@outlook.com>
2014-01-16 21:55:56 +0100

    début des bouchons
    mieux vaut tard que jamais aight

--- Caisse/src/fr/epita/sigl/miwa/application/messaging/AsyncMessageListener.java
+++ Caisse/src/fr/epita/sigl/miwa/application/messaging/AsyncMessageListener.java
@@ -10,6 +10,14 @@ import fr.epita.sigl.miwa.st.async.message.AAsyncMessageListener;
 public class AsyncMessageListener extends AAsyncMessageListener {
 	private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
+	/*
+	liste des messages/fichiers :
+	FA - BO vers Caisse (articles, prix et promotions)
+	MA - BO vers Caisse (mise à jour des articles, prix et promotions en cours de journée)
+	MS - Caisse vers BO (ticket de vente au fil de l’eau)
+	FA - Caisse vers BO (tous les tickets de vente en fin de journée)
+	*/
+	
 	@Override
 	public void onException(Exception e) {
 		// TODO Auto-generated method stub
@@ -18,13 +26,18 @@ public class AsyncMessageListener extends AAsyncMessageListener {
 
 	@Override
 	public void onMessage(String message, EApplication source) {
-		LOGGER.severe(message);
+		if (source == EApplication.BACK_OFFICE){
+			LOGGER.info("Message reçu du Back Office");
+			LOGGER.info("Le message est : " + message);
+		}
 	}
 
 	@Override
 	public void onFile(File file, EApplication source) {
-		LOGGER.severe(source + " : " + file.getAbsolutePath());	
-		
+		if (source == EApplication.BACK_OFFICE){
+			LOGGER.info("Fichier reçu du Back Office");
+			LOGGER.info("Le path du fichier est : " + file.getAbsolutePath());
+		}
 	}
 
 }
