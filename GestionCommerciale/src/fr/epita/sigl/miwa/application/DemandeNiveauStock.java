03e1ebedac397a28baeaf0d0f96b5a0c7019c131
seb92140 <seb.cormier@free.fr>
2014-01-16 15:05:03 -0500

    ajout demandeniveaustock

---
+++ GestionCommerciale/src/fr/epita/sigl/miwa/application/DemandeNiveauStock.java
@@ -0,0 +1,42 @@
+package fr.epita.sigl.miwa.application;
+
+import java.util.List;
+
+public class DemandeNiveauStock {
+	
+	private String commandNumber;
+	private String datedemand;
+	private String daterep;
+	private List<Articles> articles;
+	private List<String> quantity;
+	public String getCommandNumber() {
+		return commandNumber;
+	}
+	public void setCommandNumber(String commandNumber) {
+		this.commandNumber = commandNumber;
+	}
+	public String getDatedemand() {
+		return datedemand;
+	}
+	public void setDatedemand(String datedemand) {
+		this.datedemand = datedemand;
+	}
+	public String getDaterep() {
+		return daterep;
+	}
+	public void setDaterep(String daterep) {
+		this.daterep = daterep;
+	}
+	public List<Articles> getArticles() {
+		return articles;
+	}
+	public void setArticles(List<Articles> articles) {
+		this.articles = articles;
+	}
+	public List<String> getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(List<String> quantity) {
+		this.quantity = quantity;
+	}
+}
