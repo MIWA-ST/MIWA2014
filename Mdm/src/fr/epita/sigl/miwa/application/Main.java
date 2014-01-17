5ea455b7c368a1f8140f0b7e0de2b8dff4bf08a6
Romain email = Romainredm <riding_r@epita.fr>
2014-01-17 13:24:06 +0100

    Test envoie de fichier au BI

--- Mdm/src/fr/epita/sigl/miwa/application/Main.java
+++ Mdm/src/fr/epita/sigl/miwa/application/Main.java
@@ -49,7 +49,7 @@ public class Main {
 			e.printStackTrace();
 		}
 		
-		//AsyncFileFactory.getInstance().getFileHelper().send("outputFileGC.xml", EApplication.GESTION_COMMERCIALE);
+		AsyncFileFactory.getInstance().getFileHelper().send("outputFileBI.xml", EApplication.BI);
 		
 		XmlReader xmlReader = new XmlReader("testFileGC.xml");
 		xmlReader.parseProducts();
