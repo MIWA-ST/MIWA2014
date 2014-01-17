a668682ac86a4e9f5ed7d2c7efdcc45fc163ef51
Romain email = Romainredm <riding_r@epita.fr>
2014-01-17 12:19:00 +0100

    Update Promotion.java

--- Mdm/src/fr/epita/sigl/miwa/application/Promotion.java
+++ Mdm/src/fr/epita/sigl/miwa/application/Promotion.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package fr.epita.sigl.miwa.application;
 
 import java.util.Date;
@@ -29,34 +28,4 @@ public class Promotion {
 	
 
 }
-=======
-package fr.epita.sigl.miwa.application;
 
-import java.util.Date;
-
-public class Promotion {
-	private Date date_debut;
-	private Date date_fin;
-	private int pourcentage;
-	
-	public Date getDate_debut() {
-		return date_debut;
-	}
-	public void setDate_debut(Date date_debut) {
-		this.date_debut = date_debut;
-	}
-	public Date getDate_fin() {
-		return date_fin;
-	}
-	public void setDate_fin(Date date_fin) {
-		this.date_fin = date_fin;
-	}
-	public double getPourcentage() {
-		return pourcentage;
-	}
-	public void setPourcentage(int pourcentage) {
-		this.pourcentage = pourcentage;
-	}
-
-}
->>>>>>> a0bea0f089ac98df235bc67373647853749233b6
