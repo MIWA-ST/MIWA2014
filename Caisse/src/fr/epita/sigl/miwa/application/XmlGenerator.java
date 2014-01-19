f88c7aab8eabe3980f09fb4ce519f85ee09d1bc5
Fabien Patou <fabien.patou.junk@outlook.com>
2014-01-19 02:17:27 +0100

    fonction pour gérer demande paiement CB

--- Caisse/src/fr/epita/sigl/miwa/application/XmlGenerator.java
+++ Caisse/src/fr/epita/sigl/miwa/application/XmlGenerator.java
@@ -1,17 +1,28 @@
 package fr.epita.sigl.miwa.application;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.logging.Logger;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
 import fr.epita.sigl.miwa.application.clock.ClockClient;
 import fr.epita.sigl.miwa.st.EApplication;
 import fr.epita.sigl.miwa.st.async.message.AsyncMessageFactory;
 import fr.epita.sigl.miwa.st.async.message.exception.AsyncMessageException;
+import fr.epita.sigl.miwa.st.sync.SyncMessFactory;
 
 public class XmlGenerator {
-	private static final Logger LOGGER = Logger.getLogger(XmlGenerator.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(XmlGenerator.class
+			.getName());
 
 	public String GetCurrentClockDate() {
 		Calendar cal = Calendar.getInstance();
@@ -21,6 +32,48 @@ public class XmlGenerator {
 				+ Integer.toString(cal.get(Calendar.DAY_OF_MONTH));
 
 		return theDate;
+	}
+
+	public boolean CheckCbPaymentWithMo(String total, String numCB,
+			String date, String picto) {
+		String message = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><monetique service=\"paiement_cb\"><montant>"
+				+ total
+				+ "</montant><cb><numero>"
+				+ numCB
+				+ "</numero><date_validite>"
+				+ date
+				+ "</date_validite><pictogramme>"
+				+ picto
+				+ "</pictogramme></cb></monetique>";
+
+		DocumentBuilderFactory fabriqueD = DocumentBuilderFactory.newInstance();
+		DocumentBuilder constructeur;
+		Document document = null;
+		try {
+			constructeur = fabriqueD.newDocumentBuilder();
+			File fileXml = new File(message);
+			document = constructeur.parse(fileXml);
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		LOGGER.info("***** Caisse : envoi d'une demande de paiement CB vers la monétique");
+		boolean result = SyncMessFactory.getSyncMessSender().sendXML(
+				EApplication.MONETIQUE, document);
+		
+		if (result)
+			LOGGER.info("***** Caisse : le paiement CB a été approuvé par la monétique");
+		else
+			LOGGER.info("***** Caisse : erreur, le paiement CB n'a pas été approuvé par la monétique");
+		
+		return result;
 	}
 
 	public void SendTicketToBO(Set<Produit> produits, String idClient,
@@ -42,11 +95,12 @@ public class XmlGenerator {
 		}
 
 		xml += "</TICKETVENTE>";
-		
+
 		System.out.println(xml);
-		
+
 		try {
-			AsyncMessageFactory.getInstance().getAsyncMessageManager().send(xml, EApplication.BACK_OFFICE);
+			AsyncMessageFactory.getInstance().getAsyncMessageManager()
+					.send(xml, EApplication.BACK_OFFICE);
 			LOGGER.info("***** Caisse : ticket de fin de vente envoyé au back-office");
 		} catch (AsyncMessageException e1) {
 			LOGGER.info("***** Caisse : erreur, l'envoi du ticket de fin de vente au back-office a généré une exception");
