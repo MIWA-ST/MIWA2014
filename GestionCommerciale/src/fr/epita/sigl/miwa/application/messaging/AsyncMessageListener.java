0f46286cd425723207415456d8c6eeb3b4618a84
Loic <ferrei_l@epita.fr>
2014-01-20 17:45:33 +0100

    algo stock

--- GestionCommerciale/src/fr/epita/sigl/miwa/application/messaging/AsyncMessageListener.java
+++ GestionCommerciale/src/fr/epita/sigl/miwa/application/messaging/AsyncMessageListener.java
@@ -3,6 +3,9 @@ package fr.epita.sigl.miwa.application.messaging;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -22,8 +25,10 @@ import fr.epita.sigl.miwa.application.DemandeReassort;
 import fr.epita.sigl.miwa.application.JdbcConnection;
 import fr.epita.sigl.miwa.application.Main;
 import fr.epita.sigl.miwa.application.Promotions;
+import fr.epita.sigl.miwa.application.StockEntrepot;
 import fr.epita.sigl.miwa.application.StockMagasin;
 import fr.epita.sigl.miwa.application.XMLManager;
+import fr.epita.sigl.miwa.application.clock.ClockClient;
 import fr.epita.sigl.miwa.st.EApplication;
 import fr.epita.sigl.miwa.st.async.message.AAsyncMessageListener;
 import fr.epita.sigl.miwa.st.async.message.AsyncMessageFactory;
@@ -64,20 +69,77 @@ public class AsyncMessageListener extends AAsyncMessageListener {
 					for (Articles a : demandereassort.getArticles()) {
 						LOGGER.info("*****: Article : " + a.getRef_article());
 					}
-					// FIXME vérifier l'état des stocks et traiter une commande
-					// fournisseur ou pas
-					// Commande fournisseur + envoi bon de commande fournisseur
-					// à l'entrepot => JE SAIS PAS QUOI METTRE EN PARAMETRE
-					// content =
-					// XMLManager.getInstance().envoicommandefournisseurtoEntrepot(commande);
-					// AsyncMessageFactory.getInstance().getAsyncMessageManager().send(content,
-					// EApplication.ENTREPOT);
-					// LOGGER.info("Envoi de la commande fournisseur à l'entrepot");
+					JdbcConnection.getInstance().getConnection();
+					List<StockEntrepot> stock = JdbcConnection.getInstance()
+							.envoi_all_stock();
+					JdbcConnection.getInstance().closeConnection();
 
-					// Demande Reassort
-					// Stock Entrepot Suffisant ?
-					// Si oui > Suite / decrément
-					// Si non > Commande Fournisseur / incrémente
+					List<Articles> articlefournisseur = new ArrayList<Articles>();
+					List<String> quantitefoutnisseur = new ArrayList<String>();
+					int j = 0;
+					while (j < demandereassort.getArticles().size()) {
+						int i = 0;
+						boolean finded = false;
+						Articles currentarticle = demandereassort.getArticles()
+								.get(j);
+						while (i < stock.size()) {
+							StockEntrepot currentstock = stock.get(i);
+							if (stock.get(i).getRef_article()
+									.equals(currentarticle.getRef_article())) {
+								finded = true;
+								if (Integer
+										.parseInt(currentstock.getQuantity()) <= Integer
+										.parseInt(demandereassort.getQuantity()
+												.get(j))) {
+									int quantity = Integer
+											.parseInt(currentstock
+													.getQuantity())
+											- Integer.parseInt(demandereassort
+													.getQuantity().get(j)) + 20;
+									articlefournisseur.add(currentarticle);
+									quantitefoutnisseur.add(Integer
+											.toString(quantity));
+								}
+							}
+							i++;
+							if (!finded) {
+								StockEntrepot newstock = new StockEntrepot();
+								newstock.setRef_article(demandereassort
+										.getArticles().get(j).getRef_article());
+								newstock.setQuantity("0");
+								JdbcConnection.getInstance().getConnection();
+								JdbcConnection.getInstance()
+										.insertStockEntrepot(newstock);
+								JdbcConnection.getInstance().closeConnection();
+								int quantity = Integer.parseInt(demandereassort
+										.getQuantity().get(j)) + 30;
+								articlefournisseur.add(demandereassort
+										.getArticles().get(j));
+								quantitefoutnisseur.add(Integer
+										.toString(quantity));
+							}
+						}
+
+						if (!articlefournisseur.isEmpty()) {
+							CommandeFournisseur commandef = new CommandeFournisseur();
+							commandef.setArticles(articlefournisseur);
+							commandef.setquantity(quantitefoutnisseur);
+							commandef.setTraitee("FALSE");
+							commandef.setNumero_commande(ClockClient.getClock()
+									.getHour().toString());
+							DateFormat df = new SimpleDateFormat("yyyyMMdd");
+							commandef.setBon_commande(df.format(ClockClient
+									.getClock().getHour()));
+							content = XMLManager.getInstance()
+									.envoicommandefournisseurtoEntrepot(
+											commandef);
+							AsyncMessageFactory.getInstance()
+									.getAsyncMessageManager()
+									.send(content, EApplication.ENTREPOT);
+						}
+						j++;
+					}
+
 					LOGGER.severe("*****:Préparation de l'envoi de la demande de réassort à l'ENTREPOT : "
 							+ demandereassort.getCommandNumber());
 					content = XMLManager.getInstance()
@@ -87,6 +149,43 @@ public class AsyncMessageListener extends AAsyncMessageListener {
 							.send(content, EApplication.ENTREPOT);
 					LOGGER.severe("*****:demande de réassort envoyé à l'entrepot :"
 							+ demandereassort.getCommandNumber());
+					// decrémenter stock
+					int i = 0;
+					while (i < demandereassort.getArticles().size()) {
+						boolean finded = false;
+						int y = 0;
+						while (y < stock.size()
+								&& stock.get(y)
+										.getArticle()
+										.getRef_article()
+										.equals(demandereassort.getArticles()
+												.get(i).getRef_article())) {
+							finded = true;
+							int newquantity = Integer.parseInt(stock.get(y)
+									.getQuantity())
+									- Integer.parseInt(demandereassort
+											.getQuantity().get(i));
+							stock.get(y).setQuantity(
+									Integer.toString(newquantity));
+							JdbcConnection.getInstance().getConnection();
+							JdbcConnection.getInstance().modif_stock(
+									stock.get(y).getArticle(),
+									stock.get(y).getQuantity());
+							JdbcConnection.getInstance().closeConnection();
+							y++;
+						}
+						if (!finded) {
+							StockEntrepot newstock = new StockEntrepot();
+							newstock.setRef_article(demandereassort
+									.getArticles().get(i).getRef_article());
+							newstock.setQuantity("0");
+							JdbcConnection.getInstance().getConnection();
+							JdbcConnection.getInstance().insertStockEntrepot(
+									newstock);
+							JdbcConnection.getInstance().closeConnection();
+						}
+						i++;
+					}
 
 				} else if (root.toLowerCase().equals("demandeniveaudestock")) {
 					LOGGER.severe("*****: Reception des niveaux de stock depuis BO");
@@ -107,8 +206,8 @@ public class AsyncMessageListener extends AAsyncMessageListener {
 					int i = 0;
 					while (i < demand.getArticles().size()) {
 						LOGGER.info("*****: Confirmation Article :"
-								+ demand.getArticles().get(i).getRef_article() + " quantite :"
-								+ demand.getQuantity().get(i));
+								+ demand.getArticles().get(i).getRef_article()
+								+ " quantite :" + demand.getQuantity().get(i));
 						i++;
 					}
 					LOGGER.severe("*****: Fin Confirmation réassort depuis BO :"
@@ -128,6 +227,46 @@ public class AsyncMessageListener extends AAsyncMessageListener {
 					LOGGER.severe("*****: FIN Bon de livraison commande fournisseur recu : "
 							+ cmd.getNumero_commande());
 					// FIXME incrémenter les stocks
+					JdbcConnection.getInstance().getConnection();
+					List<StockEntrepot> stock = JdbcConnection.getInstance()
+							.envoi_all_stock();
+					JdbcConnection.getInstance().closeConnection();
+					
+					int i = 0;
+					while (i < cmd.getArticles().size()) {
+						boolean finded = false;
+						int y = 0;
+						while (y < stock.size()
+								&& stock.get(y)
+										.getArticle()
+										.getRef_article()
+										.equals(cmd.getArticles()
+												.get(i).getRef_article())) {
+							finded = true;
+							int newquantity = Integer.parseInt(stock.get(y)
+									.getQuantity())
+									+ Integer.parseInt(cmd
+											.getquantity().get(i));
+							stock.get(y).setQuantity(
+									Integer.toString(newquantity));
+							JdbcConnection.getInstance().getConnection();
+							JdbcConnection.getInstance().modif_stock(
+									stock.get(y).getArticle(),
+									stock.get(y).getQuantity());
+							JdbcConnection.getInstance().closeConnection();
+							y++;
+						}
+						if (!finded) {
+							StockEntrepot newstock = new StockEntrepot();
+							newstock.setRef_article(cmd.getArticles().get(i).getRef_article());
+							newstock.setQuantity(cmd.getquantity().get(i));
+							JdbcConnection.getInstance().getConnection();
+							JdbcConnection.getInstance().insertStockEntrepot(
+									newstock);
+							JdbcConnection.getInstance().closeConnection();
+						}
+						i++;
+					}
 				}
 
 				else if (root.toLowerCase().equals("expeditionclient")) {
@@ -142,6 +281,46 @@ public class AsyncMessageListener extends AAsyncMessageListener {
 					}
 					LOGGER.severe("*****: FIN expedition client recu : "
 							+ cmd.getCommandNumber());
+					
+					//DECREMENTER STOCK
+					JdbcConnection.getInstance().getConnection();
+					List<StockEntrepot> stock = JdbcConnection.getInstance()
+							.envoi_all_stock();
+					JdbcConnection.getInstance().closeConnection();
+					int i = 0;
+					while (i < cmd.getArticles().size()) {
+						boolean finded = false;
+						int y = 0;
+						while (y < stock.size()
+								&& stock.get(y)
+										.getArticle()
+										.getRef_article()
+										.equals(cmd.getArticles()
+												.get(i).getRef_article())) {
+							finded = true;
+							int newquantity = Integer.parseInt(stock.get(y)
+									.getQuantity())
+									- Integer.parseInt(cmd.getquantity().get(i));
+							stock.get(y).setQuantity(
+									Integer.toString(newquantity));
+							JdbcConnection.getInstance().getConnection();
+							JdbcConnection.getInstance().modif_stock(
+									stock.get(y).getArticle(),
+									stock.get(y).getQuantity());
+							JdbcConnection.getInstance().closeConnection();
+							y++;
+						}
+						if (!finded) {
+							StockEntrepot newstock = new StockEntrepot();
+							newstock.setRef_article(cmd.getArticles().get(i).getRef_article());
+							newstock.setQuantity("0");
+							JdbcConnection.getInstance().getConnection();
+							JdbcConnection.getInstance().insertStockEntrepot(
+									newstock);
+							JdbcConnection.getInstance().closeConnection();
+						}
+						i++;
+					}
 				}
 
 			} else if (source == EApplication.INTERNET) {
