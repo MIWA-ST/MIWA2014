58a86caf4aee9a686e1bb55f6add501bf6c2a738
Benoît <benoit.rigault@gmail.com>
2014-01-19 22:27:02 -0500

    Main modifié

--- CRM/src/fr/epita/sigl/miwa/application/Main.java
+++ CRM/src/fr/epita/sigl/miwa/application/Main.java
@@ -47,14 +47,15 @@ public class Main {
 		
 		//XMLManager.getInstance().getSegmentationClient("coucou");
 		Client.clientsList = new ArrayList<Client>();
-		JdbcConnection.getInstance().getConnection();
+		System.out.println(XMLManager.getInstance().getDemandeSegmentationClient("essai"));
+		//JdbcConnection.getInstance().getConnection();
 		//XMLManager.getInstance().getDemandeCreationCompte("lol", "creation compte internet.xml");
-		XMLManager.getInstance().getCreationTypeCarte("Silver");
-		XMLManager.getInstance().getCreationTypeCarte("Gold");
-		XMLManager.getInstance().getTicketClientFidelise("lol", "BO ticket client fidelise.xml");
-		XMLManager.getInstance().getSegmentationClient("lol", "segmentation-client.xml");
-		XMLManager.getInstance().dispatchXML("coucou", "BO ticket caisse.xml");
-		JdbcConnection.getInstance().GetClientInternet("831356");
+		//XMLManager.getInstance().getCreationTypeCarte("Silver");
+		//XMLManager.getInstance().getCreationTypeCarte("Gold");
+		//XMLManager.getInstance().getTicketClientFidelise("lol", "BO ticket client fidelise.xml");
+		//XMLManager.getInstance().getSegmentationClient("lol", "segmentation-client.xml");
+		//XMLManager.getInstance().dispatchXML("coucou", "BO ticket caisse.xml");
+		//JdbcConnection.getInstance().GetClientInternet("831356");
 		//System.out.println(ClockClient.getClock().getHour());
 		//ClockClient.getClock().wakeMeUpEveryDays(date, "baseclient");
 		
