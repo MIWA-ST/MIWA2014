6d72e000da32965146d15f7b5f3a59e87f93ebce
ST Bastien <fiorentino.bastien@gmail.com>
2014-01-27 15:39:13 +0100

    RMI Timeout 5s

--- IN/src/fr/epita/sigl/miwa/st/Conf.java
+++ IN/src/fr/epita/sigl/miwa/st/Conf.java
@@ -8,6 +8,7 @@ import java.net.UnknownHostException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.server.RMISocketFactory;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -79,6 +80,11 @@ public class Conf {
 
 	private Conf() {
 		try {
+			RMISocketFactory.setSocketFactory(new TimedSocketFactory(5000));
+		} catch (IOException e) {
+			LOGGER.severe("failed to set RMIFactory.");
+		}
+		try {
 			_prop.load(new FileInputStream("conf/config.properties"));
 			_currentApplication = EApplication.getFromShortName(_prop
 					.getProperty("application"));
@@ -145,8 +151,8 @@ public class Conf {
 	}
 
 	/**
-	 * Retourne le chemin vers le rÃ©pertoire local oÃ¹ les fichiers asynchrones
-	 * vont Ãªtre stockÃ©s
+	 * Retourne le chemin vers le répertoire local où les fichiers asynchrones
+	 * vont être stockés
 	 * 
 	 * @return
 	 * @throws ConfigurationException
